a337a76381eeedd1d013c18bb912a4dac779211c
José Ben Hur <joseben.hur.86@gmail.com>
2023-04-03 09:59:09 -0300

    update logic year

--- jsx/script.js
+++ jsx/script.js
@@ -1,8 +1,21 @@
-if(document.getElementById("anoAtual") && document.getElementById("idadeAtual")){
+if (
+  document.getElementById("anoAtual") &&
+  document.getElementById("idadeAtual")
+) {
+  function getAge(dateString) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+
+    if (m < 0 || (m === 0) & (today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
   let anoAtual = new Date().getFullYear();
-document.getElementById("anoAtual").innerHTML = anoAtual;
-let idadeAtual = 2023-1986;
-document.getElementById("idadeAtual").innerHTML = idadeAtual;
+  document.getElementById("anoAtual").innerHTML = anoAtual;
+  document.getElementById("idadeAtual").innerHTML = getAge("1986/05/15");
 }
 let nav = document.querySelector("#navFooter").clientHeight;
 let footer = document.getElementById("footer");
